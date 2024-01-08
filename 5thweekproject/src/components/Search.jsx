e4a4d5b953d920fe23438c38f8a830aaa66f7bed
Tania Boterman <taniaboterman@gmail.com>
2024-01-08 08:22:57 +0100

    searchfunctionailty working

--- 5thweekproject/src/components/Search.jsx
+++ 5thweekproject/src/components/Search.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
 import PropTypes from "prop-types"
 
 //declare functional component - in a func component what you return is rendered
 export default function Search({setSearch}) {
     return (
         <input 
-        //setting type of input field
         type="text"
         onChange={e => setSearch(e.target.value)} 
-        //event handler updates search state when input/value changes
         />
-    );
-
-    Results.propTypes = {
-        data: PropTypes.array
-}
-
+        );
+    }
+    
+    Search.propTypes = {
+        setSearch: PropTypes.func.isRequired
+    }
+    
+    
+//setting type of input field
+//event handler updates search state when input/value changes
 //event hanlder is an arrow function that receives a the event object e as an arg
 //calls set search with curr val of input field
 // which is assoc with e.target.value
